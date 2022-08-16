ead787c858ec902ef3e328dbcb73935322f6d2ca
Хрімлі Віктор Вікторович <viktorhrimli101@gmail.com>
2022-08-16 17:49:58 +0300

    change

--- src/enemyCountry.js
+++ src/enemyCountry.js
@@ -1,7 +1,7 @@
 export function enemyCountry(official, svg, languages, capital, population) {
   return ` <div class="conteiner">
   <img width="50" height="50" src="https://upload.wikimedia.org/wikipedia/commons/c/c5/MoscowPride.jpg" alt="" />
-      <h2 class="country__text--head">расия</h2>
+      <h2 class="country__text--head"><span>р</span>асия</h2>
       </div>
       <ul>
         <li>
