759495d7a7fae68d093fb92f39c5e9a70bb4e932
Хрімлі Віктор Вікторович <viktorhrimli101@gmail.com>
2022-08-16 18:00:32 +0300

    Change

--- src/enemyCountry.js
+++ src/enemyCountry.js
@@ -1,19 +1,18 @@
 export function enemyCountry(official, svg, languages, capital, population) {
   return ` <div class="conteiner">
-  <img width="50" height="50" src="https://upload.wikimedia.org/wikipedia/commons/c/c5/MoscowPride.jpg" alt="" />
-      <h2 class="country__text--head"><span>р</span>асия</h2>
+  <img width="50" height="50" src="https://adm.dp.gov.ua/storage/app/media/uploaded-files/symbol/gerb_max.jpg" alt="" />
+      <h2 class="country__text--head">Дніпропетровська область</h2>
       </div>
       <ul>
         <li>
-           <p><b>Capital:</b>${capital} скоро сгорит нахуй</p>
+           <p><b>Capital:</b>${capital} це Україна</p>
         </li>
           <li>
-           <p><b>Population:</b>${population} 86% пидорасов</p>
+           <p><b>Population:</b>${population} поїхали до сибіру</p>
         </li>
           <li>
            <p><b>Languages:</b> ${languages} незабаром українська</p>
         </li>
       </ul>
-      <h1>Все буде Україна</h1>
       `;
 }
